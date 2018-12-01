ef9aeb3bcee39b9c8ec5cc922879373fda3340d6
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-12-01 17:55:20 +0100

    configured id showing

---
+++ src/main/java/com/codecool/restflights/Config/RepositoryConfiguration.java
@@ -0,0 +1,15 @@
+package com.codecool.restflights.Config;
+
+import com.codecool.restflights.Model.Airport;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Airport.class);
+    }
+}
