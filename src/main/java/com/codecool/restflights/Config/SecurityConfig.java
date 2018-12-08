f1bd8b1945f35de9cd327510f22dc8bcdaca6283
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-12-08 18:48:37 +0100

    smth works

--- src/main/java/com/codecool/restflights/Config/SecurityConfig.java
+++ src/main/java/com/codecool/restflights/Config/SecurityConfig.java
@@ -4,9 +4,15 @@ import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -18,12 +24,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private String issuer;
 
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors();
         JwtWebSecurityConfigurer
                 .forRS256(apiAudience, issuer)
-                .configure(http);
+                .configure(http)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/airports").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/routes").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/search").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/passengers").authenticated();
+//                .antMatchers(HttpMethod.GET, "/api/private-scoped").hasAuthority("read:messages");
     }
 }
